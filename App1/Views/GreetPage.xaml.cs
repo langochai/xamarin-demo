ef33089e0690a9b3b1e3981ed697a2bbbc90a19f
La Ngoc Hai <langochai11121998@gmail.com>
2024-07-03 11:46:57 +0700

    1st commit

---
+++ App1/Views/GreetPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        private string[] questions = { "no1no1no1no1no1no1no1no1no1no1no1", "no2no2no2no2no2no2no2no2no2no2no2", "no3no3no3no3no3no3no3no3no3no3no3" };
+        private string[] colors = { "red", "blue", "purple", "green", "yellow", "brown" };
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
